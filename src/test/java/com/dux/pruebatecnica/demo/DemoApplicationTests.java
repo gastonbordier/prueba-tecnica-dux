9a4bbfe2e8573b60885d070a5f7fc85db61faf20
Gaston Bordier <gaston.bordier@rcpo.com.ar>
2024-09-04 18:14:52 -0300

    primer commit

---
+++ src/test/java/com/dux/pruebatecnica/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dux.pruebatecnica.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
